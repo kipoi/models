ea52c0c9e925b614d0359156658d8f76747858e7
xnancy <xnancy@stanford.edu>
2018-02-28 19:51:21 -0800

    Add BassetGM12878_Demo model for transfer learning

--- BassetGM12878_Demo/dataloader.py
+++ BassetGM12878_Demo/dataloader.py
@@ -1,40 +1,43 @@
 from __future__ import absolute_import, division, print_function
-import six
-import os
-import inspect
-from builtins import str, open, range, dict
 
-import pickle
 import numpy as np
+import pandas as pd
 import pybedtools
-
+from pybedtools import BedTool
 from genomelake.extractors import FastaExtractor
-from kipoi.metadata import GenomicRanges
 from kipoi.data import Dataset
+from kipoi.metadata import GenomicRanges
+import linecache
 
-filename = inspect.getframeinfo(inspect.currentframe()).filename
-DATALOADER_DIR = os.path.dirname(os.path.abspath(filename))
 
-# File paths
-intervals_file = "example_files/intervals_files_ENCSR000EMT_chr21_10000.tsv"
-fasta_file = "example_files/chr21.fa"
+class BedToolLinecache(BedTool):
+    """Faster BedTool accessor by Ziga Avsec
 
-class FastaDataset(Dataset):
+    Normal BedTools loops through the whole file to get the
+    line of interest. Hence the access it o(n)
+
+    Note: this might load the whole bedfile into memory
+    """
+
+    def __getitem__(self, idx):
+        line = linecache.getline(self.fn, idx + 1)
+        return pybedtools.create_interval_from_list(line.strip().split("\t"))
+
+
+class SeqDataset(Dataset):
     """
     Args:
-        intervals_file: file path; tsv file
-            Assumes bed-like `chrom start end label` format.
+        intervals_file: bed3+1 file containing intervals+labels
         fasta_file: file path; Genome sequence
     """
 
     def __init__(self, intervals_file, fasta_file):
         # intervals
-        self.bt = pybedtools.BedTool(intervals_file)
-
-        # extractors
-        self.input_data_extractors = {"data/genome_data_dir": FastaExtractor(fasta_file)}
-
-        self.target_dataset = None
+        #if use_linecache:
+         #   self.bt = BedToolLinecache(intervals_file)
+        #else:
+        self.bt = BedTool(intervals_file)
+        self.fasta_extractor = FastaExtractor(fasta_file)
 
     def __len__(self):
         return len(self.bt)
@@ -42,34 +45,23 @@ class FastaDataset(Dataset):
     def __getitem__(self, idx):
         interval = self.bt[idx]
 
-        out = {}
+        # Intervals need to be 600bp wide
+        assert interval.stop - interval.start == 1000
 
-        out['inputs'] = {key: np.squeeze(extractor([interval]), axis=0)
-                         for key, extractor in self.input_data_extractors.items()}
+        if interval.name is not None:
+            y = np.array([float(interval.name)])
+        else:
+            y = {}
 
-        
-        #if self.target_dataset is not None:
-        out["targets"] = np.array([float(interval.name)])
+        # Run the fasta extractor
+        seq = np.squeeze(self.fasta_extractor([interval]))
 
-        # get metadata
-        out['metadata'] = {}
-        out['metadata']['ranges'] = {}
-        out['metadata']['ranges']['chr'] = interval.chrom
-        out['metadata']['ranges']['start'] = interval.start
-        out['metadata']['ranges']['end'] = interval.stop
-        out['metadata']['ranges']['id'] = interval.name
-        #out['metadata']['ranges']['strand'] = interval.strand
-
-        return out
-
-# test batching
-# from torch.utils.data import DataLoader
-
-# dl = DataLoader(a, batch_size=3, collate_fn=numpy_collate)
-
-# it = iter(dl)
-# sample = next(it)
-
-# sample["inputs"]["dist_polya_st"].shape
-# sample["inputs"]["seq"].shape
-
+        # Reformat so that it matches the Basset shape
+        # seq = np.swapaxes(seq, 1, 0)[:,:,None]
+        return {
+            "inputs": {"data/genome_data_dir": seq},
+            "targets": y,
+            "metadata": {
+                "ranges": GenomicRanges.from_interval(interval)
+            }
+        }
--- BassetGM12878_Demo/readme.md
+++
@@ -1,6 +0,0 @@
-## Checklist
-
-- [ ] answer all the quesionts
-- [ ] dump the serialized model to ...
-  - TODO - make it easy to export?
-- [ ] dump the serialized model to ...
