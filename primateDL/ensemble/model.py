37bfda9ebd3100c4c8cdfb34e2b5c2a4a308cce0
lsundaram <laksshman@gmail.com>
2018-06-20 17:26:55 -0700

    softlink

---
+++ primateDL/ensemble/model.py
@@ -0,0 +1,17 @@
+from kipoi.model import KerasModel, BaseModel
+
+
+class PrimateDL(BaseModel):
+    def __init__(self):
+        mdir = "model_files"
+        models = [KerasModel(weights=os.path.join(mdir, "0weights{i}.hdf5".format(i=i),
+                             arch=os.path.join(mdir, "model.json"),
+                             backend='tensorflow')
+                  for i in range(8)]
+
+    def predict_on_batch(self, x):
+        return np.stack([m.predict_on_batch(x) 
+                         for m in self.models]).mean(axis=0)
+
+
+
