7648d3fd57def50934835b52acadd26bcaaa275c
Žiga Avsec <Avsecz@users.noreply.github.com>
2018-10-27 12:28:23 +0200

    use linecache by default

--- DeepSEA/template/dataloader.py
+++ DeepSEA/template/dataloader.py
@@ -64,7 +64,7 @@ class SeqDataset(Dataset):
     SEQ_WIDTH = 1000
 
     def __init__(self, intervals_file, fasta_file,
-                 target_file=None, use_linecache=False):
+                 target_file=None, use_linecache=True):
 
         # intervals
         if use_linecache:
