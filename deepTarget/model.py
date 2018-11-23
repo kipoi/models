c50a1543b5f87fea5279d47a8fbdebc1d3f0a0a5
Avsecz <zigaavsec@gmail.com>
2018-11-23 17:58:11 +0100

    fix deepTarget backend [ci skip]

--- deepTarget/model.py
+++ deepTarget/model.py
@@ -9,6 +9,8 @@ sys.path.append(this_path)
 from kipoi.model import BaseModel
 from keras.models import model_from_json
 import bio_utils
+import keras.backend as K
+K.set_image_dim_ordering("th")
 
 
 class Model(BaseModel):
