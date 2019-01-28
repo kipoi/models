fba7bd9f53581580d02eb62868d682d591edcb84
Hasan Celik <muhammedhasancelik@gmail.com>
2019-01-28 15:10:18 +0100

    bug fix #139 and refactoring

--- MMSplice/deltaLogitPSI/model.py
+++ MMSplice/deltaLogitPSI/model.py
@@ -1,16 +1,10 @@
 from kipoi.model import BaseModel
-import numpy as np
 from mmsplice import LINEAR_MODEL
 from mmsplice.utils.postproc import transform
 
-# Model to predict delta logit PSI
-class MMSpliceModel(BaseModel):
-    
-    def __init__(self):
 
-        self.model = LINEAR_MODEL
-    
+class MMSpliceModel(BaseModel):
+    '''Model to predict delta logit PSI'''
+
     def predict_on_batch(self, inputs):
-        X = transform(inputs, False)
-        pred = self.model.predict(X)
-        return pred
+        return LINEAR_MODEL.predict(transform(inputs, False))
