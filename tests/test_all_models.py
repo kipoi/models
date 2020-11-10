bd83bbe394df45ba7cf61d2a39afa02ba23a7701
Haimasree <haimasree.de@gmail.com>
2020-11-10 21:46:22 +0100

    Update test_all_models.py

--- tests/test_all_models.py
+++ tests/test_all_models.py
@@ -54,8 +54,17 @@ def test_model(model_name, caplog):
 
     source_name = "kipoi"
     assert source_name == "kipoi"
-
-    env_name = conda_env_name(model_name, model_name, source_name)
+    
+    model_to_shared_env_name_dict = {
+      "DeepBind/Homo_sapiens/TF/D00817.001_ChIP-seq_TBP" : "kipoi-shared__envs__kipoi-py3-keras2",
+      "FactorNet/CTCF/metaGENCODE_RNAseq_Unique35_DGF": "kipoi-shared__envs__kipoi-py3-keras2",
+      "HAL": "kipoi-shared__envs__kipoi-py3-keras2",
+      "SiSp": "kipoi-shared__envs__kipoi-py3-keras2",
+      "labranchor":" kipoi-shared__envs__kipoi-py3-keras2",
+      "lsgkm-SVM/Tfbs/Nanogsc33759/H1hesc/Haib_V0416102": "kipoi-shared__envs__kipoi-py3-keras2"      
+    }
+    
+    env_name = model_to_shared_env_name_dict.get(model_name, conda_env_name(model_name, model_name, source_name))
     env_name = "test-" + env_name  # prepend "test-"
 
     # if environment already exists, remove it
