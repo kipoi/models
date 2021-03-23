609f532fd3082637f316658ad5555b5e64ac443d
Kalin Nonchev <nonchev@ouga03.cmm.in.tum.de>
2021-03-23 11:06:25 +0100

    make feature type not default as this is important when querying the gtf

--- Framepool/VariantEffect/dataloader.py
+++ Framepool/VariantEffect/dataloader.py
@@ -1,12 +1,13 @@
 from kipoiseq.dataloaders import SingleVariantUTRDataLoader
 
 class SingleVariantFramepoolDataloader(SingleVariantUTRDataLoader):
-    def __init__(self, gtf_file, fasta_file, vcf_file, vcf_file_tbi=None, infer_from_cds=False, on_error_warn=True, **kwargs):
-        kwargs["feature_type"] = "5UTR"
+    def __init__(self, gtf_file, fasta_file, vcf_file, feature_type, vcf_file_tbi=None, infer_from_cds=False, on_error_warn=True, **kwargs):
+        # please specify your "feature_type" as given in the GTF file which you are using!
         super().__init__(
             gtf_file=gtf_file,
             fasta_file=fasta_file,
             vcf_file=vcf_file,
+            feature_type=feature_type,
             vcf_file_tbi=vcf_file_tbi,
             infer_from_cds=infer_from_cds,
             on_error_warn=on_error_warn,
